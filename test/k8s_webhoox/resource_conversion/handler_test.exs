acf1542aa945aa9c878fdb5149d0c3f3f2490f7e
Michael Ruoss <michael@michaelruoss.ch>
2023-04-09 12:51:21 +0200

    test error case

--- test/k8s_webhoox/resource_conversion/handler_test.exs
+++ test/k8s_webhoox/resource_conversion/handler_test.exs
@@ -6,16 +6,16 @@ defmodule K8sWebhoox.ResourceConversion.HandlerTest do
   alias K8sWebhoox.Test.ResourceConversionHelper
   alias K8sWebhoox.Test.ResourceHelper
 
-  @desired_api_version "example.com/v1beta1"
-  @api_version "example.com/v1"
+  @desired_api_version "example.com/v1"
+  @api_version "example.com/v1beta1"
   @first_kind "FirstResource"
   @second_kind "SecondResource"
 
   defmodule TestHandler do
     use K8sWebhoox.ResourceConversion.Handler
 
-    @desired_api_version "example.com/v1beta1"
-    @api_version "example.com/v1"
+    @desired_api_version "example.com/v1"
+    @api_version "example.com/v1beta1"
     @first_kind "FirstResource"
     @second_kind "SecondResource"
 
@@ -31,6 +31,13 @@ defmodule K8sWebhoox.ResourceConversion.HandlerTest do
           @desired_api_version
         ) do
       {:ok, put_in(object, ~w(metadata labels), %{"foo" => "second"})}
+    end
+
+    def convert(
+          %{"apiVersion" => "example.com/v1alpha1", "kind" => @first_kind} = object,
+          @desired_api_version
+        ) do
+      {:error, "Can't convert v1alpha1 to v1"}
     end
   end
 
@@ -50,5 +57,13 @@ defmodule K8sWebhoox.ResourceConversion.HandlerTest do
     assert is_list(converted_objects)
     assert %{"foo" => "first"} == Enum.at(converted_objects, 0)["metadata"]["labels"]
     assert %{"foo" => "second"} == Enum.at(converted_objects, 1)["metadata"]["labels"]
+  end
+
+  test "returns failure in case of error" do
+    first_resource = ResourceHelper.resource("example.com/v1alpha1", @first_kind)
+    request = ResourceConversionHelper.webhook_request(@desired_api_version, [first_resource])
+    result = TestHandler.call(Conn.new(request), [])
+    assert "Failed" == result.response["result"]["status"]
+    assert "Can't convert v1alpha1 to v1" == result.response["result"]["message"]
   end
 end
